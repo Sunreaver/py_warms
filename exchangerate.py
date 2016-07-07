b8cc0f1086039314369e1d275e139aaae81b5f18
TheOne <tanwei.rush@gmail.com>
2016-07-07 11:09:53 +0800

    数据库存储结果

--- exchangerate.py
+++ exchangerate.py
@@ -2,6 +2,7 @@
 
 import requests
 import re
+import pymongo
 
 m = requests.get("http://fx.cmbchina.com/hq/")
 if m.status_code != requests.codes.ok:
@@ -10,7 +11,7 @@ if m.status_code != requests.codes.ok:
 
 reg = re.compile(r"""
 <tr>\s*<td\s+class="fontbold">\s*(?P<name>\S+)\s*</td>\s*
-<td\s+align="center">\s*\d+\s*</td>\s*
+<td\s+align="center">\s*(?P<init>\d+)\s*</td>\s*
 <td\s+align="center"\s+class="fontbold">\s*(?P<base>\S+)\s*</td>\s*
 <td\s*class="numberright">\s*(?P<midPrice>\d+\.\d+)\s*</td>\s*
 <td\s*class="numberright">\s*(?P<sellPrice>\d+\.\d+)\s*</td>\s*
@@ -22,5 +23,33 @@ reg = re.compile(r"""
 
 rows = reg.findall(m.text)
 
+rate = None
+connection = None
+try:
+    connection = pymongo.MongoClient("localhost", 27017)
+    connection.Stocks.authenticate("stocks", "1111", timeout=10)
+    dbcl = connection.Stocks.rate
+except Exception as e:
+    print("mongo err:", e)
+    exit(0)
+
+dic = [{}]
 for v in rows:
-    print(v)
+    dic.append({
+               'name': v[0],
+               'init': v[1],
+               'base': v[2],
+               'midPrice': v[3],
+               'sellPrice': v[4],
+               'time': v[8]
+               })
+
+if len(dic) > 0:
+    try:
+        rate.insert(dic)
+    except Exception as e:
+        print("mongo insert err:", e)
+        raise Exception('MongoInsertErr')
+    finally:
+        print("OK at:", dic[0]['time'])
+connection.close()
